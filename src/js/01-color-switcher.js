a1503a5139be2745538ad729d687317fd4da8020
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-02 23:21:47 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let intervalId;
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  startBtn.setAttribute('disabled', '');
+  intervalId = setInterval(setElementBgColor, 1000, body);
+}
+
+function setElementBgColor(el) {
+  el.style.backgroundColor = getRandomHexColor();
+}
+
+function onStop() {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-// <button type="button" data-start>Start</button>
-// <button type="button" data-stop>Stop</button>
