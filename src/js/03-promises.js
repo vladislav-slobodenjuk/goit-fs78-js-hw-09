887574eab935504f3c951b3d0ded3b019c37f33e
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-05 16:30:49 +0300

    task-03 not by task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const result = {};
+
+  formData.forEach((value, name) => (result[name] = Number(value)));
+  console.log('fomdata:', result);
+  const { delay, step, amount } = result;
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay, step).then(onResolve, onReject);
   }
+}
+
+function onResolve(text) {
+  return Notify.success(text);
+}
+
+function onReject(text) {
+  return Notify.failure(text);
+}
+
+function createPromise(position, delay, step) {
+  const shouldResolve = Math.random() > 0.3;
+  const time = delay + position * step - step;
+  console.log(position, time);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        return resolve(`✅ Fulfilled promise ${position} in ${time}`);
+      } else {
+        return reject(`❌ Rejected promise ${position} in ${time}`);
+      }
+    }, time);
+  });
 }
