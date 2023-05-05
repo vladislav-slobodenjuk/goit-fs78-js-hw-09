3d2638df19b2aa3912b18733337187ad913b3cc7
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-05 18:17:59 +0300

    task-03 refactor to task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,38 +7,45 @@ formEl.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
 
-  const formData = new FormData(e.target);
   const result = {};
-
+  const formData = new FormData(e.target);
   formData.forEach((value, name) => (result[name] = Number(value)));
   console.log('fomdata:', result);
   const { delay, step, amount } = result;
 
   for (let position = 1; position <= amount; position += 1) {
-    createPromise(position, delay, step).then(onResolve, onReject);
+    const currentDelay = delay + (position * step - step);
+    createPromise(position, currentDelay).then(onResolve).catch(onCatch);
   }
 }
 
-function onResolve(text) {
-  return Notify.success(text);
-}
-
-function onReject(text) {
-  return Notify.failure(text);
-}
-
-function createPromise(position, delay, step) {
+function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  const time = delay + position * step - step;
-  console.log(position, time);
+  console.log(`position: ${position} delay: ${delay}`);
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        return resolve(`✅ Fulfilled promise ${position} in ${time}`);
+        return resolve({ position, delay });
       } else {
-        return reject(`❌ Rejected promise ${position} in ${time}`);
+        return reject({ position, delay });
       }
-    }, time);
+    }, delay);
   });
 }
+
+function onResolve({ position, delay }) {
+  return Notify.success(`✅ Fulfilled promise ${position} in ${delay}`, opt);
+}
+
+function onCatch({ position, delay }) {
+  return Notify.failure(`❌ Rejected promise ${position} in ${delay}`, opt);
+}
+
+const opt = {
+  timeout: 5000,
+  fontSize: '14px',
+  cssAnimationDuration: 500,
+  cssAnimationStyle: 'zoom',
+  // closeButton: true,
+};
